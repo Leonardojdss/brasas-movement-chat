8bc090e0872790e2f3a8f6958de7b5dd30c656f7
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-12-08 17:43:38 -0300

    inseri a pesquisa de conteudo indexado

--- ms_embedding_documents/repositories/connection_ai_search.py
+++ ms_embedding_documents/repositories/connection_ai_search.py
@@ -13,33 +13,41 @@ pdf_teste = "C:/Users/leona/brasas-movement-chat-ia/ms-embedding-documents/arqui
 conteudo = extract_pdf(pdf_teste)
 print(conteudo)
 
-# # Configurações do recurso
-# ENDPOINT = os.getenv("ENDPOINT_AI_SEARCH")
-# API_KEY = os.getenv("AZURE_AI_SEARCH_KEY")
-# INDEX_NAME = "brasas-documents"
+# Configurações do recurso
+ENDPOINT = os.getenv("ENDPOINT_AI_SEARCH")
+API_KEY = os.getenv("AZURE_AI_SEARCH_KEY")
+INDEX_NAME = "brasas-documents"
 
-# def indexar_arquivos(arquivo):
-#     search_client = SearchClient(endpoint=ENDPOINT, index_name=INDEX_NAME, credential=AzureKeyCredential(API_KEY))
-#     documentos = []
+def indexar_arquivos(arquivo):
+    search_client = SearchClient(endpoint=ENDPOINT, index_name=INDEX_NAME, credential=AzureKeyCredential(API_KEY))
+    documentos = []
+
+    #extrair titulo do arquivo
+    titulo = arquivo.split("/")[-1]
     
     
-#     if arquivo.endswith(".txt"):
-#         conteudo = extrair_txt(arquivo)
-#     elif arquivo.endswith(".pdf"):
-#         conteudo = extrair_pdf(arquivo)
-#     else:
-#         print(f"Formato não suportado: {arquivo}")
+    if arquivo.endswith(".txt"):
+        conteudo = extrair_txt(arquivo)
+    elif arquivo.endswith(".pdf"):
+        conteudo = extrair_pdf(arquivo)
+    else:
+        print(f"Formato não suportado: {arquivo}")
 
-#     documentos.append({
-#         "id": str(len(documentos) + 1),
-#         "titulo": arquivo,
-#         "conteudo": conteudo,
-#         "data": datetime.utcnow().isoformat() + "Z",
-#     })
+    documentos.append({
+        "id": str(len(documentos) + 1),
+        "titulo": arquivo,
+        "conteudo": conteudo,
+        "data": datetime.utcnow().isoformat() + "Z",
+    })
 
-#     # Indexar os documentos
-#     if documentos:
-#         resultado = search_client.upload_documents(documents=documentos)
-#         print("Documentos indexados:", resultado)
-#     else:
-#         print("Nenhum documento para indexar.")
+    # Indexar os documentos
+    if documentos:
+        resultado = search_client.upload_documents(documents=documentos)
+        print("Documentos indexados:", resultado)
+    else:
+        print("Nenhum documento para indexar.")
+
+def pesquisar_conteudo_indexado():
+    search_client = SearchClient(endpoint=ENDPOINT, index_name=INDEX_NAME, credential=AzureKeyCredential(API_KEY))
+    resultado = search_client.search(search_text="brasas", top=5)
+    print("Documentos encontrados:", resultado)
