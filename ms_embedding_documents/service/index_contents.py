ea4f94cf6590e969d3b35d337cdfc6e5c43ec9f8
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-12-15 20:43:44 -0300

    Inclusão da pesquisa vetorial

--- ms_embedding_documents/service/index_contents.py
+++ ms_embedding_documents/service/index_contents.py
@@ -3,7 +3,6 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from datetime import datetime
 from service.extract_contents import extract_pdf, extract_txt
-import random
 from dotenv import load_dotenv
 from repositories.connection_openai import connection_openai_Embeddings
 from repositories.connection_postgreSQL import connection_postgreSQL
@@ -12,11 +11,11 @@ import json
 
 load_dotenv()
 
+# converter o conteudo do arquivo em um vetor
 def embedd_documents(conteudo):
     embeddings = connection_openai_Embeddings()
     vectors = embeddings.embed_documents([conteudo])
     return vectors[0]
-
 
 def indexar_arquivos(arquivo):
 
@@ -37,9 +36,10 @@ def indexar_arquivos(arquivo):
     else:
         print(f"Formato não suportado: {arquivo}")
 
+    # chunk texto
     text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlap=20,
+    chunk_size=200,
+    chunk_overlap=60,
     length_function=len,
     is_separator_regex=False)
 
@@ -52,23 +52,28 @@ def indexar_arquivos(arquivo):
         documentos.append({
             "id": datetime.now().strftime("%d%m%Y%H%M%S"),  # Gera um ID com o formato dia, mês, ano, hora, minuto, segundo
             "titulo_do_arquivo": titulo,
-            "conteudo": json.dumps(conteudo_embedding)
+            "conteudo_vetorizada": json.dumps(conteudo_embedding),
+            "metadado": text.page_content # Adiciona o conteúdo que está sendo vetorizado
             })
 
     # # enviar os documentos para ia search
     # search_client.upload_documents(documents=documentos)
     # print("Documentos indexados com sucesso.")
 
-    # inserir os documentos no banco de dados postgresql com vector extension   
+    # inserir os documentos no banco de dados postgresql com vector extension  
+    result = "success" 
     for documento in documentos:
         try:
             cur.execute(
-                f"INSERT INTO brasas_vector_db (id, titulo, conteudo) VALUES ('{documento['id']}', '{documento['titulo_do_arquivo']}', '{documento['conteudo']}')"
+                f"INSERT INTO brasas_vector_db (id, titulo, conteudo_vetorizada, metadado) VALUES ('{documento['id']}', '{documento['titulo_do_arquivo']}', '{documento['conteudo_vetorizada']}', '{documento['metadado']}')"
             )
             conn.commit()
         except Exception as e:
             print("Documentos indexados não foram salvos no banco, erro: ", e)
-    print("Documentos indexados com sucesso.")
+        result = "failure"
+    
+    if result == "sucess":
+        print("Documentos indexados com sucesso.")
 
 # teste
 arquivo = "C:/Users/leona/brasas-movement-chat-ia/ms_embedding_documents/arquivos_teste/brasas_nossa_missao.pdf"
--- ms_embedding_documents/service/search_contents.py
+++ ms_embedding_documents/service/search_contents.py
@@ -1,13 +1,40 @@
-from azure.core.credentials import AzureKeyCredential
-from azure.search.documents import SearchClient
-from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.indexes.models import SearchIndex, SimpleField, SearchableField
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from repositories.connection_postgreSQL import connection_postgreSQL
+from repositories.connection_openai import connection_openai_Embeddings
+import os
+import json
 
-def pesquisar_conteudo_indexado(pergunta):
-    search_client = SearchClient(endpoint=ENDPOINT, index_name=INDEX_NAME, credential=AzureKeyCredential(API_KEY))
-    resultado = search_client.search(search_text=pergunta, top=5)
-    print("Documentos encontrados:", resultado)
+# Converter a pergunta em um vetor
+def embedd_pergunta(pergunta):
+    embeddings = connection_openai_Embeddings()
+    vectors = embeddings.embed_documents([pergunta])
+    return vectors[0]
 
-pergunta = "nossa missão"
+# Realizar a pesquisa de conteúdo indexado
+def pesquisar_conteudo_indexado(pergunta_vetorizada):
 
-pesquisar_conteudo_indexado(pergunta)
+    pergunta_vetorizada = json.dumps(pergunta_vetorizada)
+
+    USER_POSTGRESQL = os.getenv("USER_POSTGRESQL")
+    PASSWORD_POSTGRESQL = os.getenv("PASSWORD_POSTGRESQL")
+    ENDPOINT_POSTGRESQL = os.getenv("ENDPOINT_POSTGRESQL")
+
+    conn, cur = connection_postgreSQL(USER_POSTGRESQL, PASSWORD_POSTGRESQL, ENDPOINT_POSTGRESQL)
+
+    try:
+        cur.execute("SELECT conteudo_vetorizada <=> %s AS distance, metadado \
+                    FROM brasas_vector_db \
+                    ORDER BY distance ASC \
+                    LIMIT 10", (pergunta_vetorizada,))
+        results = cur.fetchall()
+        conn.commit()
+        return results
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+        
+pergunta = "quais são todas as 5 missões do brasas"
+pergunta_ve=embedd_pergunta(pergunta)
+print(pesquisar_conteudo_indexado(pergunta_ve))
