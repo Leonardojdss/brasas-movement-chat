c45fff0fa8726d29e9c7744f53a9e797bb63c3cd
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-12-16 01:38:30 -0300

    Update main.py
    Signed-off-by: Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>

--- front_end_streamlit/app/main.py
+++ front_end_streamlit/app/main.py
@@ -22,7 +22,7 @@ def client_openai(messages, contexto):
 
 def get_contexto_brasas(fala_usuario):
     body = {"text": f"{fala_usuario}"}
-    request = requests.post("https://ms-embedding-documents.azurewebsites.net/ms_embedding/search_similarity", json=body)
+    request = requests.post("https://host_app_service/ms_embedding/search_similarity", json=body)
     contexto = request.json()
     return contexto
 
